5afb14ac347899526161138f1281ec05fd3dea01
zojohnson2 <113068631+zojohnson2@users.noreply.github.com>
2023-09-22 15:36:24 -0400

    Add files via upload

---
+++ dummy.R
@@ -0,0 +1,10 @@
+Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
