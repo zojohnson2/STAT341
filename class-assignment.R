72bd12438c3dabad8aa62c838d03a903cba618aa
zojohnson2 <113068631+zojohnson2@users.noreply.github.com>
2023-09-25 15:31:02 -0400

    Add files via upload

---
+++ class-assignment.R
@@ -0,0 +1,52 @@
+Pearson's product-moment correlation
+
+data:  my_data$drat and my_data$qsec
+t = 0.50164, df = 30, p-value = 0.6196
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.265947  0.426340
+sample estimates:
+       cor 
+0.09120476 
+Pearson's product-moment correlation
+
+data:  my_data$hp and my_data$gear
+t = -0.69402, df = 30, p-value = 0.493
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.4544774  0.2332119
+sample estimates:
+       cor 
+-0.1257043 
+Pearson's product-moment correlation
+
+data:  my_data$gear and my_data$carb
+t = 1.5609, df = 30, p-value = 0.129
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.08250603  0.56844218
+sample estimates:
+      cor 
+0.2740728 
+
+	Pearson's product-moment correlation
+
+data:  my_data$disp and my_data$wt
+t = 10.576, df = 30, p-value = 1.222e-11
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ 0.7811586 0.9442902
+sample estimates:
+      cor 
+0.8879799 
+Pearson's product-moment correlation
+
+data:  my_data$mpg and my_data$disp
+t = -8.7472, df = 30, p-value = 9.38e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9233594 -0.7081376
+sample estimates:
+       cor 
+-0.8475514 
+
