ccf1e02d92a7bb73a5671b4ac3294d54a550a01b
zojohnson2 <113068631+zojohnson2@users.noreply.github.com>
2023-10-09 15:36:21 -0400

    Add files via upload

---
+++ descriptive_variable_funstions.R
@@ -0,0 +1,33 @@
+variable <-c(1,23,4,5,6,7,8,9,10,1900)
+
+# Calculate mean for variable1
+
+mean(variable1)
+
+# Calculate median for variable1
+
+median(variable1)
+
+# Calculate standard deviation for variable1
+
+sd(variable1)
+
+# Calculate Mode for variable1
+
+#Install following library in R
+
+install.packages('DescTools')
+
+# Load the library
+
+library(DescTools)
+
+Mode(variable1)
+
+#Calculate range/Min and Max Value
+
+min(variable1)
+max(variable1)
+
+#calculate variance in R
+var(variable1)
